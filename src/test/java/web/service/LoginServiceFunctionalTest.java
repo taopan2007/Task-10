f295e7e8b3fea197182c0fa49121a8dac9df8756
TaoPan <TaoPan@DESKTOP-ITEVP7E>
2024-05-23 17:37:23 +0930

    DRIVER

--- src/test/java/web/service/LoginServiceFunctionalTest.java
+++ src/test/java/web/service/LoginServiceFunctionalTest.java
@@ -9,6 +9,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.Assert.*;
 
@@ -17,7 +22,8 @@ public class LoginServiceFunctionalTest {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "D:\\Programs\\chromedriver-win64\\chromedriver.exe");
+       // System.setProperty("webdriver.chrome.driver", "D:\\Programs\\chromedriver-win64\\chromedriver.exe");
+    	WebDriverManager.getInstance(CHROME).setup();
         driver = new ChromeDriver();
         driver.get("http://127.0.0.1:8080/login");
         System.out.println("Page title is: " + driver.getTitle());
@@ -53,7 +59,7 @@ public class LoginServiceFunctionalTest {
         wait.until(ExpectedConditions.urlContains("login"));
 
         String errorMessage = driver.findElement(By.id("error")).getText();
-        assertEquals("Incorect credentials", errorMessage);
+        assertEquals("Incorrect credentials", errorMessage);
     }
 
     @After
